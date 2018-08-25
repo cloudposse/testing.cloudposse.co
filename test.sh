a652fe39acc234a8eca8dc0593b1acf3ec456027
aknysh <andriy.knysh@gmail.com>
2018-08-25 11:11:38 -0400

    Fix typo

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Example of running container as though we were in CI
 
 docker run --rm --name testing \
@@ -6,8 +8,10 @@ docker run --rm --name testing \
 	-e AWS_PROFILE=default \
 	-e AWS_ACCESS_KEY_ID \
 	-e AWS_SECRET_ACCESS_KEY \
-        -e AWS_SESSION_TOKEN \
+    -e AWS_SESSION_TOKEN \
 	-e AWS_SECURITY_TOKEN \
 	-e AWS_REGION \
 	-e AWS_DEFAULT_REGION \
-	-e VAULT_SERVER_ENABLED=false --volume $(pwd)/testing/:/conf/testing/ cloudposse/testing.cloudposse.co:latest -l -c "testing/run.sh"
+	-e VAULT_SERVER_ENABLED=false \
+	--volume $(pwd)/testing/:/conf/testing/ \
+	cloudposse/testing.cloudposse.co:latest -l -c "tests/run.sh"
