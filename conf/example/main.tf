49bde2b3de7f6ce745c15661d2bb97dfffa61b69
Erik Osterman <erik@cloudposse.com>
2019-04-11 03:01:26 -0700

    Add example

---
+++ conf/example/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 0.11.2"
+
+  backend "s3" {}
+}
+
+variable "aws_assume_role_arn" {
+  type = "string"
+}
+
+provider "aws" {
+  assume_role {
+    role_arn = "${var.aws_assume_role_arn}"
+  }
+}
+
+
+resource "aws_s3_bucket" "default" {
+  bucket = "codefresh-gitops-example"
+  acl    = "private"
+
+  tags = {
+    Name        = "Codefresh GitOps Example"
+  }
+}
